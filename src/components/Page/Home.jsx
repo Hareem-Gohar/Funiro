9ef42bf3d3dc1b8f5cb05f48e573ad665745ff2a
Hareem Gohar <malikzarsh2@gmail.com>
2024-12-21 21:26:53 +0500

    Error solved

--- src/components/Page/Home.jsx
+++ src/components/Page/Home.jsx
@@ -1,33 +1,30 @@
-import React from 'react'
-import Header from '../layout/header'
-import HeroSection from '../Home/HeroSection'
-import Services from '../Home/Services'
-import ProductList from '../Product/ProductList'
-import Inspiration from '../Home/inspiration'
-import TipsAndTricks from '../Home/Tips-n-Tricks'
-import FuniroFurniture from '../Home/FuniroFurniture'
-import Footer from '../layout/Footer'
+import React from "react";
+import Header from "../layout/Header";
+import HeroSection from "../Home/HeroSection";
+import Services from "../Home/Services";
+import ProductList from "../Product/ProductList";
+import Inspiration from "../Home/inspiration";
+import TipsAndTricks from "../Home/Tips-n-Tricks";
+import FuniroFurniture from "../Home/FuniroFurniture";
+import Footer from "../layout/Footer";
 
 const Home = () => {
-    return (
-        <>
-            <div className="bg-[#F9F1E7]">
-                <Header />
-                <HeroSection />
+  return (
+    <>
+      <div className="bg-[#F9F1E7]">
+        <Header />
+        <HeroSection />
+      </div>
+      <div className="w-full max-w-7xl mx-auto px-6">
+        <Services />
+      </div>
+      <ProductList />
+      <Inspiration />
+      <TipsAndTricks />
+      <FuniroFurniture />
+      <Footer />
+    </>
+  );
+};
 
-            </div>
-            <div className="w-full max-w-7xl mx-auto px-6">
-             <Services />    
-            </div>
-            <ProductList />
-            <Inspiration />
-            <TipsAndTricks />
-            <FuniroFurniture />
-            <Footer />
-           
-        </>
-
-    )
-}
-
-export default Home
+export default Home;
